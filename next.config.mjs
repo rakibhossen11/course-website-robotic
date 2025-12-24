2b16ac67288e275a6c93bcb5e3fbb076526953f7
rakibhossen11 <rakibhossen1664@gmail.com>
2025-12-24 11:18:19 +0600

    some update

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        // Optional: port: '', (default is fine)
+        pathname: '**',  // Allows any path under this domain
+      },
+    ],
+  },
+  reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'lh3.googleusercontent.com',
-//         // Optional: port: '', (default is fine)
-//         pathname: '**',  // Allows any path under this domain
-//       },
-//     ],
-//   },
-//   reactCompiler: true,
-//   async headers() {
-//     return [
-//       {
-//         source: '/:path*',
-//         headers: [
-//           {
-//             key: 'Cross-Origin-Opener-Policy',
-//             value: 'same-origin-allow-popups',
-//           },
-//           {
-//             key: 'Cross-Origin-Embedder-Policy',
-//             value: 'unsafe-none',
-//           },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {

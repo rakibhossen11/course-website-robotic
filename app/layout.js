6e0169a99beb99f5547a36cbffb010d126bdeed9
rakibhossen11 <rakibhossen1664@gmail.com>
2025-12-24 10:13:27 +0600

    add suspence on rootlayout

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,13 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { InvoiceProvider } from './contexts/InvoiceContext';
+import { Suspense } from 'react';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Suspense>
         <AuthProvider>
           <InvoiceProvider>
           <Navbar />
@@ -15,6 +17,7 @@ export default function RootLayout({ children }) {
           </InvoiceProvider>
         </AuthProvider>
         <Footer />
+        </Suspense>
       </body>
     </html>
   );
